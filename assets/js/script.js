ab14b57e63cf661e6398d1dc1b343f130f7d011a
Thayná Simões <thayna.pereira2402@gmail.com>
2024-10-09 21:25:50 -0300

    projeto completo

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+const horas = document.getElementById('horas');
+
+const minutos = document.getElementById('minutos');
+
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (seg < 10) seg = '0' + seg;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+})
+
+
+
+function mudarCor() {
+
+    let inputPrimeiraCor = document.getElementById('primeira_cor').value;
+    let inputSegundaCor = document.getElementById('segunda_cor').value;
+
+    // OUTRA OPÇÃO
+    
+    let gradient = `linear-gradient(120deg, ${inputPrimeiraCor + "," + inputSegundaCor})`;
+
+    //let gradient = "linear-gradient(120deg ," + inputPrimeiraCor + "," + inputSegundaCor + ")";
+
+    document.body.style.background = gradient;
+}
